3818fe20a50ed08623a9416abff62bfc873cb3d2
Soy Zen <noobiezz2020@gmail.com>
2024-06-26 09:35:26 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+
+<?php
+
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "portfolio";
+
+    $conn = mysqli_connect($server, $user, $password, $database);
+
+?>
